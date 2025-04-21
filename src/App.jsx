169fdef555aa67b6c6efd46f9588bf038c1a00a8
Ed <edca81@gmail.com>
2025-04-21 14:25:57 -0600

    REACT essentials
    Chapter 3 steps: components creation as JS functions that return JSX code to cast HTML code into the app. Props to pass dynamic content as parameters to component functions, children prop is native for component content, also props for events (ie onClick). State definition for initial content and dynamic changes. States 'tell' react to render a component again with new props

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
-import atomImg from "./assets/react-core-concepts.png"
+// useState hook for default render
+import { useState } from "react";
+
 import componentsImg from "./assets/components.png"
-
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescription = ['Fundamental', 'Core', 'Crucial'];
-
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescription[genRandomInt(2)];
-  return (
-  <header>
-    <img src={atomImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {description} React concepts you will need for almost any app you are going
-      to build!
-    </p>
-  </header>
-  )
-}
-
-function CoreConcept({image, title, description}){
-  return (
-    <li>
-      <img src={image} alt={title+'_img'} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import { EXAMPLES } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
+  // hooks MUST be called from top level of the component
+  const [selectedTopic, setSelectedTopic] = useState();
+  
+  // event handler for button click
+  function handleSelect(selectedButton){
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  // tabContent setup
+  let tabContent = <p>Please select a topic</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -41,17 +39,28 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            {/* MAP function to translate JSON to component */}
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
           </ul>
         </section>
-
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton 
+            isSelected={selectedTopic === 'components'}
+            onSelect={()=>handleSelect('components')}>Components</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === 'jsx'}
+            onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === 'props'}
+            onSelect={()=>handleSelect('props')}>PROPS</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === 'state'}
+            onSelect={()=>handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
